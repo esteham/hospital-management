ca1f3e0a8e2a6a568cd889a66ea37a95996fe0a2
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-11-03 23:13:09 +0600

    added allowed origins link

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,12 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+                    'http://localhost:5173',
+                    'http://127.0.0.1:5173',
+                    'https://hospital.xetroot.com',
+                    ],
+
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
